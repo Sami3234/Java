9c6f117263576e715c16e5c01ea6b948c6380a04
Sami3234 <kk34samikhan@gmail.com>
2025-03-24 21:46:04 +0500

    submit

--- work/src/lab1.java
+++ work/src/lab1.java
@@ -1,10 +1,15 @@
-import java.until.Scaner;
+import java.until.Scanner;
 
 public class lab1 {
     public static void main(String[] args) {
-        Scanner se = new Scaner(System.in);
-        int a = sc.nextInt();
-        System.out.println();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Reg No :: ");
+        Int reg = sc.nextInt();
+        if (reg >= 50) {
+            System.out.println("Your Section is A");
+        } else {
+            System.out.println("your section is B");
+        }
     }
 }
