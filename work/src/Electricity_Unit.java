1daf9a0238b0ed9970dbfa3b051e9def1d0d1767
Muhammad Sami Ullah <kk34samikhan@gmail.com>
2025-03-26 17:52:12 +0500

    submit

--- work/src/Electricity_Unit.java
+++ work/src/Electricity_Unit.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter the number of Units: ");
         int units = sc.nextInt();
-        if(units<=50){
-            units = units*0.50;
-            System.out.println("The total amount is : "+units);
-        }
-        else if (units<=100){
-            units = units*0.75
-            System.out.println("The total amount is : "+units);
+        double bill = 0.0;
 
-
+        if (units <= 50) {
+            bill = units * 0.50;
+        } else if (units <= 150) {
+            bill = (50 * 0.50) + ((units - 50) * 0.75);
+        } else if (units <= 250) {
+            bill = (50 * 0.50) + (100 * 0.75) + ((units - 150) * 1.20);
+        } else {
+            bill = (50 * 0.50) + (100 * 0.75) + (100 * 1.20) + ((units - 250) * 1.50);
         }
-        
+
+        // Adding surcharge of 20%
+        double surcharge = bill * 0.20;
+        double totalBill = bill + surcharge;
+
+        System.out.printf("Your electricity bill (before surcharge) is: %.2f%n", bill);
+        System.out.printf("Surcharge (20%%): %.2f%n", surcharge);
+        System.out.printf("Your total electricity bill is: %.2f%n", totalBill);
         sc.close();
     }
 }
