d11fbed29ccb6a79cae60c70d819198bd3f2cf5b
Muhammad Sami Ullah <kk34samikhan@gmail.com>
2025-04-05 13:12:50 +0500

    update

---
+++ work/src/for_loop.java
@@ -0,0 +1,8 @@
+//this is a simple for looop
+  public class for_loop {
+        public static void main(String[] args) {
+              for (int i = 0; i>-0; i++) {
+                System.out.println(i);
+              }  
+            }
+        }
