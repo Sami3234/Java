6acab892f8ea6ec7f7b5cb598d908753286e4d3e
Muhammad Sami Ullah <kk34samikhan@gmail.com>
2025-04-07 17:51:33 +0500

    update

--- work/src/for_loop.java
+++ work/src/for_loop.java
@@ -1,7 +1,7 @@
 //this is a simple for looop
   public class for_loop {
         public static void main(String[] args) {
-              for (int i = 0; i>-0; i++) {
+              for (int i = 0; i<0; i++) {
                 System.out.println(i);
               }  
             }
